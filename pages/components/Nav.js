393103c6bec3873535e003eee2daff2c7cc651ba
Patrick <sawthedeveloper@gmail.com>
2022-10-27 23:29:40 -0700

    Update steady

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export default function Nav () {
   return (
-    <div>
-        <h1>Nav</h1>
+    <nav className=" flex justify-between items-center py-10">
+    <Link href="/">
+    <button className="text-lg font-medium">Creative Thoughts</button>
+    </Link>
 
-    </div>
+      <ul className="flex items-center gap-10 ">
+
+        <Link href={"/auth/login"}>
+            <h2 className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">Join Now</h2>
+        </Link>
+      </ul>
+    </nav>
   );
 }
